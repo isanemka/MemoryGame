87b47750da600057a7a896862decf9acf66ec35c
Maxibahrami <maxibahrami@gmail.com>
2024-01-21 20:08:12 +0600

    Json Server Created to Load images Via Api Call

--- memory.js
+++ memory.js
@@ -1,135 +1,160 @@
-NiceSelect.bind(document.getElementById('timeSelect'));
+// Load Dynamic Images
+document.addEventListener('DOMContentLoaded', function () {
+  // Server images Urls
+  let serverUrl = 'http://localhost:9000/images'
 
-const cards = document.querySelectorAll('.card');
-let flippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-let timerInterval;
-let timeRemaining = 30;
-let timerStarted = false;
-let count = 0;
-const restart = document.getElementById('restart');
+  function loadImages() {
+    fetch(serverUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        var selectedCards = document.querySelectorAll('li.card[data-img]')
+        selectedCards.forEach(function (card) {
+          var dataImgValue = card.getAttribute('data-img')
+          var imgFront = card.querySelector('.img-front')
+          var imgBack = card.querySelector('.img-back')
+          imgFront.src = data?.find((i) => i?.name === 'qmark')?.url
+          imgBack.src = data?.find((i) => i?.name === dataImgValue)?.url
+        })
+      })
+      .catch((error) => {
+        console.error('Error loading data:', error)
+      })
+  }
+
+  loadImages()
+})
+
+NiceSelect.bind(document.getElementById('timeSelect'))
+
+const cards = document.querySelectorAll('.card')
+let flippedCard = false
+let lockBoard = false
+let firstCard, secondCard
+let timerInterval
+let timeRemaining = 30
+let timerStarted = false
+let count = 0
+const restart = document.getElementById('restart')
 
 function gameStart() {
-    cards.forEach(card => {
-        let setRandom = Math.floor(Math.random() * 12 + 1);
-        card.style.order = setRandom;
-        card.classList.remove('flip');
-        card.addEventListener('click', flipCard);
-    })
-    lockBoard = false;
-    count = 0;
+  cards.forEach((card) => {
+    let setRandom = Math.floor(Math.random() * 12 + 1)
+    card.style.order = setRandom
+    card.classList.remove('flip')
+    card.addEventListener('click', flipCard)
+  })
+  lockBoard = false
+  count = 0
 }
 
 function startTimer() {
-    if (!timerStarted) {
-        const timerDisplay = document.querySelector('.time span b');
+  if (!timerStarted) {
+    const timerDisplay = document.querySelector('.time span b')
 
-        timerInterval = setInterval(() => {
-            timeRemaining--;
-            timerDisplay.innerText = timeRemaining;
+    timerInterval = setInterval(() => {
+      timeRemaining--
+      timerDisplay.innerText = timeRemaining
 
-            if (timeRemaining === 0) {
-                clearInterval(timerInterval);
-                alert('Tiden är ute! Game over');
-                restartGame();
-            }
+      if (timeRemaining === 0) {
+        clearInterval(timerInterval)
+        alert('Tiden är ute! Game over')
+        restartGame()
+      }
+    }, 1000)
 
-        }, 1000);
-
-        timerStarted = true;
-    }
+    timerStarted = true
+  }
 }
 
 function stopTimer() {
-    clearInterval(timerInterval);
-    timerStarted = false;
-
+  clearInterval(timerInterval)
+  timerStarted = false
 }
 
 function flipCard() {
-    if (!timerStarted) {
-        startTimer();
-    }
-    if (lockBoard) return;
-    if (this === firstCard) return;
+  if (!timerStarted) {
+    startTimer()
+  }
+  if (lockBoard) return
+  if (this === firstCard) return
 
-    this.classList.add('flip');
+  this.classList.add('flip')
 
-    if (!flippedCard) {
-        flippedCard = true;
-        firstCard = this;
-        return;
-    }
+  if (!flippedCard) {
+    flippedCard = true
+    firstCard = this
+    return
+  }
 
-    secondCard = this;
-    checkForMatch();
-    count++;
-    counter();
+  secondCard = this
+  checkForMatch()
+  count++
+  counter()
 }
 
 function counter() {
-    document.querySelector('.flips span b').innerText = count;
+  document.querySelector('.flips span b').innerText = count
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.img === secondCard.dataset.img;
+  let isMatch = firstCard.dataset.img === secondCard.dataset.img
 
-    isMatch ? disableCards() : unflipCards();
+  isMatch ? disableCards() : unflipCards()
 }
 
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
 
-    resetBoard();
+  resetBoard()
 }
 
 function unflipCards() {
-    lockBoard = true;
+  lockBoard = true
 
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+  setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
 
-        resetBoard();
-    }, 1000);
+    resetBoard()
+  }, 1000)
 }
 
 function resetBoard() {
-    [flippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+  ;[flippedCard, lockBoard] = [false, false]
+  ;[firstCard, secondCard] = [null, null]
 
-    if (document.querySelectorAll('.card:not(.flip)').length === 0) {
-        setTimeout(() => {
-
-            alert('Grattis! Du har klarat spelet! Det krävdes ' + count + ' drag.');
-            restartGame();
-        }, 500);
-    }
+  if (document.querySelectorAll('.card:not(.flip)').length === 0) {
+    setTimeout(() => {
+      alert('Grattis! Du har klarat spelet! Det krävdes ' + count + ' drag.')
+      restartGame()
+    }, 500)
+  }
 }
 function changeTime() {
-    stopTimer();
-    timeRemaining = parseInt(document.getElementById("timeSelect").value);
-    document.querySelector(".time span b").innerText = timeRemaining;
-    gameStart();
+  stopTimer()
+  timeRemaining = parseInt(document.getElementById('timeSelect').value)
+  document.querySelector('.time span b').innerText = timeRemaining
+  gameStart()
 }
 
 function restartGame() {
-    stopTimer();
-    timeRemaining = parseInt(document.getElementById('TimeSelect').value);
-    gameStart();
+  stopTimer()
+  timeRemaining = parseInt(document.getElementById('TimeSelect').value)
+  gameStart()
 }
 
 function restartGame() {
-    stopTimer();
-    timeRemaining = parseInt(document.getElementById('timeSelect').value);
-    gameStart();
-    counter();
-    count = 0;
+  stopTimer()
+  timeRemaining = parseInt(document.getElementById('timeSelect').value)
+  gameStart()
+  counter()
+  count = 0
 }
 
-document.getElementById('timeSelect').addEventListener('change', changeTime);
-restart.addEventListener('click', restartGame);
+document.getElementById('timeSelect').addEventListener('change', changeTime)
+restart.addEventListener('click', restartGame)
 
-gameStart();
+gameStart()
